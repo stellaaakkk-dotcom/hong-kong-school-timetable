9dfad3b0c06065049c2d9a74334441002af6abc0
stellaaakkk-dotcom <stellaaa.kkk@gmail.com>
2026-08-25 21:40:02 +0800

    Add files via upload
    others

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = "hk-teacher-journal-pwa-v7";
+const ROOT = new URL("./", self.registration.scope).href;
+const CORE = ["./", "./manifest.webmanifest", "./app-icon-192.png", "./app-icon-512.png", "./pdf.worker.min.mjs"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener("fetch", event => {
+  const request = event.request;
+  if (request.method !== "GET" || new URL(request.url).origin !== self.location.origin) return;
+  if (request.mode === "navigate") {
+    event.respondWith(fetch(request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE).then(cache => cache.put(ROOT, copy));
+      return response;
+    }).catch(() => caches.match(ROOT)));
+    return;
+  }
+  event.respondWith(caches.match(request).then(cached => cached || fetch(request).then(response => {
+    if (response.ok) caches.open(CACHE).then(cache => cache.put(request, response.clone()));
+    return response;
+  })));
+});
